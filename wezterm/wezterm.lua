05433ad09b58e8820730c81ad94e1eb41868eee3
davidcortesortuno <david.cortes.o@gmail.com>
2021-12-24 12:00:08 +0100

    Added wezterm config

---
+++ wezterm/wezterm.lua
@@ -0,0 +1,25 @@
+local wezterm = require 'wezterm';
+return {
+  font = wezterm.font("Fira Code"),
+  color_scheme = "Desert",
+
+  keys = {
+    -- This will create a new split and run your default program inside it
+    {key="j", mods="CTRL|SHIFT",
+      action=wezterm.action{SplitVertical={domain="CurrentPaneDomain"}}},
+    {key="l", mods="CTRL|SHIFT",
+      action=wezterm.action{SplitHorizontal={domain="CurrentPaneDomain"}}},
+
+    {key="RightArrow", mods="ALT",
+      action=wezterm.action{ActivatePaneDirection="Right"}},
+
+    {key="LeftArrow", mods="ALT",
+      action=wezterm.action{ActivatePaneDirection="Left"}},
+
+    {key="DownArrow", mods="ALT",
+      action=wezterm.action{ActivatePaneDirection="Down"}},
+
+    {key="UpArrow", mods="ALT",
+      action=wezterm.action{ActivatePaneDirection="Up"}},
+  }
+}
