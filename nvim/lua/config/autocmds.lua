ebf1d67c57481e419c000f564cb2312d95570de1
davidcortesortuno <david.cortes@usm.cl>
2023-06-22 18:21:59 -0400

    nvim: updated lazyvim setup

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -1,3 +1,11 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+vim.cmd [[
+  augroup _simulation_files
+    " OOMMFs .mif files syntax colouring as a .tcl file
+    au BufRead,BufNewFile *.mif set filetype=tcl
+    " MuMax3 files with Go syntax
+    au BufRead,BufNewFile *.mx3 set filetype=go
+  augroup end
+]]
--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -1,17 +1,33 @@
 -- add pyright to lspconfig
 return {
-  "neovim/nvim-lspconfig",
-  ---@class PluginLspOpts
-  opts = {
-    ---@type lspconfig.options
-    servers = {
-      -- pyright will be automatically installed with mason and loaded with lspconfig
-      pyright = {},
+    "neovim/nvim-lspconfig",
+    ---@class PluginLspOpts
+    opts = {
+        ---@type lspconfig.options
+        servers = {
+            -- pyright will be automatically installed with mason and loaded with lspconfig
+            pyright = {},
+        },
+        setup = {
+            clangd = function(_, opts)
+                opts.capabilities.offsetEncoding = { "utf-16" }
+            end,
+        },
+        -- options for vim.diagnostic.config()
+        diagnostics = {
+            underline = true,
+            update_in_insert = false,
+            virtual_text = false, -- disable in-line diagnostics
+            --
+            -- virtual_text = {
+            --     spacing = 4,
+            --     source = "if_many",
+            --     prefix = "●",
+            --     -- this will set set the prefix to a function that returns the diagnostics icon based on the severity
+            --     -- this only works on a recent 0.10.0 build. Will be set to "●" when not supported
+            --     -- prefix = "icons",
+            -- },
+            severity_sort = true,
+        },
     },
-    setup = {
-      clangd = function(_, opts)
-        opts.capabilities.offsetEncoding = { "utf-16" }
-      end,
-    }
-  },
 }
