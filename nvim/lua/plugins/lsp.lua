5c5136b8a725efbda044507807c4570887fb89d5
davidcortesortuno <david.cortes@usm.cl>
2023-07-31 17:31:13 -0400

    nvim: improving plugins

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -7,6 +7,7 @@ return {
         servers = {
             -- pyright will be automatically installed with mason and loaded with lspconfig
             pyright = {},
+            texlab = {},
         },
         setup = {
             clangd = function(_, opts)
--- nvim/lua/plugins/null-ls.lua
+++ nvim/lua/plugins/null-ls.lua
@@ -1,21 +1,18 @@
 return {
-  "jose-elias-alvarez/null-ls.nvim",
-  event = { "BufReadPre", "BufNewFile" },
-  dependencies = { "mason.nvim" },
-  opts = function()
-    local nls = require("null-ls")
-    return {
-      root_dir = require("null-ls.utils").root_pattern(".null-ls-root", ".neoconf.json", "Makefile", ".git"),
-      sources = {
-        nls.builtins.diagnostics.ruff.with({
-          extra_args = { "--ignore=E501" },
-        }),
-        nls.builtins.completion.spell,
-        nls.builtins.diagnostics.misspell.with({
-          filetypes = { "tex", "text" },
-        }),
-        nls.builtins.diagnostics.proselint,
-      },
-    }
-  end,
+    "jose-elias-alvarez/null-ls.nvim",
+    event = { "BufReadPre", "BufNewFile" },
+    dependencies = { "mason.nvim" },
+    opts = function()
+        local nls = require("null-ls")
+        return {
+            root_dir = require("null-ls.utils").root_pattern(".null-ls-root", ".neoconf.json", "Makefile", ".git"),
+            sources = {
+                nls.builtins.diagnostics.ruff.with({
+                    extra_args = { "--ignore=E501" },
+                }),
+                nls.builtins.completion.spell,
+                nls.builtins.diagnostics.proselint,
+            },
+        }
+    end,
 }
