fa94eb8cfa1e3573336f8647e34e5464e38a93a1
davidcortesortuno <david.cortes@usm.cl>
2023-11-17 17:03:55 -0300

    nvim: updated linters and formatters

---
+++ nvim/lua/plugins/conform.lua
@@ -0,0 +1,15 @@
+return {
+    {
+        "stevearc/conform.nvim",
+        opts = {
+            formatters_by_ft = {
+                python = { "ruff" },
+            },
+            formatters = {
+                ruff = {
+                    prepend_args = { "--ignore=E501" },
+                },
+            },
+        },
+    },
+}
