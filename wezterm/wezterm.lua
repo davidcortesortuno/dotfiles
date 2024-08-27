cf20091f073788d62d302d487a2f45a00a3a429e
davidcortesortuno <david.cortes@usm.cl>
2024-08-27 15:47:07 -0400

    Updated wezterm to new lua syntaxis

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,30 +1,56 @@
-local wezterm = require 'wezterm';
-return {
-  font = wezterm.font("Fira Code"),
-  font_size = 13.0,
-  color_scheme = "Desert",
-  hide_mouse_cursor_when_typing = false,
+local wezterm = require("wezterm")
+local act = wezterm.action
+local config = {}
 
-  keys = {
+config.font = wezterm.font("Fira Code")
+config.font_size = 13.0
+config.color_scheme = "Desert"
+config.hide_mouse_cursor_when_typing = false
+
+config.keys = {
     -- This will create a new split and run your default program inside it
-    {key="j", mods="CTRL|SHIFT",
-      action=wezterm.action{SplitVertical={domain="CurrentPaneDomain"}}},
-    {key="h", mods="CTRL|SHIFT",
-      action=wezterm.action{SplitHorizontal={domain="CurrentPaneDomain"}}},
+    {
+        key = "j",
+        mods = "CTRL|SHIFT",
+        action = wezterm.action({ SplitVertical = { domain = "CurrentPaneDomain" } }),
+    },
+    {
+        key = "h",
+        mods = "CTRL|SHIFT",
+        action = wezterm.action({ SplitHorizontal = { domain = "CurrentPaneDomain" } }),
+    },
+    { key = "RightArrow", mods = "ALT", action = wezterm.action({ ActivatePaneDirection = "Right" }) },
+    { key = "LeftArrow", mods = "ALT", action = wezterm.action({ ActivatePaneDirection = "Left" }) },
+    { key = "DownArrow", mods = "ALT", action = wezterm.action({ ActivatePaneDirection = "Down" }) },
+    { key = "UpArrow", mods = "ALT", action = wezterm.action({ ActivatePaneDirection = "Up" }) },
 
-    {key="RightArrow", mods="ALT",
-      action=wezterm.action{ActivatePaneDirection="Right"}},
+    -- { key = "b", mods = "CTRL", action = wezterm.action.RotatePanes("CounterClockwise") },
 
-    {key="LeftArrow", mods="ALT",
-      action=wezterm.action{ActivatePaneDirection="Left"}},
-
-    {key="DownArrow", mods="ALT",
-      action=wezterm.action{ActivatePaneDirection="Down"}},
-
-    {key="UpArrow", mods="ALT",
-      action=wezterm.action{ActivatePaneDirection="Up"}},
-  },
-
-  -- Spawn a fish shell in login mode
-  default_prog = { '/usr/bin/fish', '-l' }
+    -- { key = "n", mods = "CTRL", action = wezterm.action.RotatePanes("Clockwise") },
+    
+    -- Disable alt+enter for fullscreen
+    {
+      key = 'Enter',
+      mods = 'ALT',
+      action = wezterm.action.DisableDefaultAssignment,
+    },
 }
+
+-- Change mouse scroll amount
+config.mouse_bindings = {
+  {
+    event = { Down = { streak = 1, button = { WheelUp = 1 } } },
+    mods = 'NONE',
+    action = act.ScrollByLine(-1),
+  },
+  {
+    event = { Down = { streak = 1, button = { WheelDown = 1 } } },
+    mods = 'NONE',
+    action = act.ScrollByLine(1),
+  },
+}
+
+-- Spawn a fish shell in login mode
+config.default_prog = { "/usr/bin/fish", "-l" }
+
+return config
